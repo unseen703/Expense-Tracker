0fc2f5691dd239b5a6a921c6501d5fc903281008
dipak <103515582+unseen703@users.noreply.github.com>
2023-06-21 21:59:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,14 +11,8 @@ dotenv.config({path: './config/config.env'});
 connectDB();
 const app = express();
 app.use(express.json());
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
-}
+
 app.use('/api/v1/transactions', transactions)
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    app.get('*', (res, req)=>res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
-}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,console.log(`server is running on ${process.env.NODE_ENV} mode on port${PORT}`.blue.bold))
