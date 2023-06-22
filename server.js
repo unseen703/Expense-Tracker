206239ded759753debad5dbb3ba0eae62e7fd384
unseen <unseem703@gmail.com>
2023-06-22 09:59:51 +0530

    added cors

--- server.js
+++ server.js
@@ -2,14 +2,23 @@ const path  = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
-const morgan = require('morgan');
 
-const transactions = require('./routes/transactions');
+const bodyParser  = require ('body-parser');
+const cors = require ('cors');
 const connectDB = require('./config/db');
-dotenv.config({path: './etc/secrets/config.env'});
+const transactions = require('./routes/transactions');
+
+const app = express();
+
+// dotenv.config({path: './etc/secrets/config.env'});
+
+dotenv.config({path: './config/config.env'});
 
 connectDB();
-const app = express();
+
+app.use(bodyParser.urlencoded({limit :"30mb", extended :true }));
+app.use(cors());
+app.use(bodyParser.json({limit :"30mb", extended :true }));
 app.use(express.json());
 
 app.use('/api/v1/transactions', transactions);
